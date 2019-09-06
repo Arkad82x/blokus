d81cb76a81c34f2cef71d2e6ac4eca7147be6440
Andre Muess <andre.muess@adindex.de>
2019-09-06 16:37:48 +0200

    added logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,8 @@ import Nav from 'react-bootstrap/Nav'
 import routes from '../routes'
 import { Link } from 'react-router-dom'
 
+import logo from './logo.jpg'
+
 const Navigation = () => (
   <Nav>
     { routes.filter(r => r.inMenu).map(({label, path}) => 
@@ -17,6 +19,15 @@ const Navigation = () => (
 export default (props) => {
   return (
       <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={Link} to="/home">
+          <img
+            src={logo}
+            width="100"
+            height="50"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
         <Navigation />
       </Navbar>
     )
