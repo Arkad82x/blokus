ad112aeee3b3c797a3bc14a46ba299d6d9fa25cb
Andre <andre.cux90@gmail.com>
2019-10-11 00:20:58 +0200

    working client server setup with mongo and node running in docker

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,24 @@ import Footer from './components/Footer'
 import routes from './routes'
 
 const App = () => {
+
+   function handleClick() {
+      fetch("/api/test")
+      .then(a => a.json())
+      .then(a => console.log(a))
+      .catch(e => console.error(e))
+   }
+
    return (
      <div>
+        <button onClick={handleClick} >request </button>
         <Header/>
        { routes.map(route => (
           <Route key={route.path} path={route.path} component={route.component} exact />
        ))}
         <Footer />
      </div>
-  )
+   )
 }
 
 export default App
