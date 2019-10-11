2f05c8ce619f912f2cd926ecac5f6b997658b252
Andre <andre.cux90@gmail.com>
2019-10-11 22:08:13 +0200

    remove api test code

--- client/src/App.js
+++ client/src/App.js
@@ -10,32 +10,15 @@ import routes from './routes'
 
 
 
-const App = () => {
-      function create() {
-         fetch("/api/games/create")
-            .then(a => a.json())
-            .then(a => console.log("CREATE", a))
-            .catch(e => console.error("Server connection test failed"))
-      }
-
-      function get() {
-         fetch("/api/games")
-            .then(a => a.json())
-            .then(a => console.log("GET: ", a))
-            .catch(e => console.error("Server connection test failed"))
-      }
-     return (
-     <div>
-        <button onClick={create}>Create</button>
-        <button onClick={get}>Get</button>
-        <Header/>
-       { routes.map(route => (
-          <Route key={route.path} path={route.path} component={route.component} exact />
-       ))}
-        <Footer />
-     </div>
-   )
-}
+const App = () => (
+   <div>
+      <Header/>
+      { routes.map(route => (
+         <Route key={route.path} path={route.path} component={route.component} exact />
+      ))}
+      <Footer />
+   </div>
+)
 
 export default App
 
